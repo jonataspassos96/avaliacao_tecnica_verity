148aaaa39970beca360214111da3dcd5bdfdf73c
Jonatas Passos <jpc080496@gmail.com>
2023-01-23 17:34:56 -0300

    feat: scripts de teste para a Home finalizados

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,1 +1,23 @@
 /// <reference types='cypress' />
+
+describe('Home', () => {
+    it('Deve ser exbido o título Nossas soluções_', () => {
+        cy.visit('/')
+
+        cy.contains('span', 'Somos Verity')
+            .should('have.text', 'Somos Verity')
+
+        cy.get('#comp-kwv1stru > p > span > span > span:nth-child(2) > span > span > span')
+            .should('have.text', '_')
+    })
+
+    it('Deve ser exbido o título Nossos clientes_', () => {
+        cy.visit('/')
+
+        cy.contains('span', 'Nossos clientes')
+            .should('have.text', 'Nossos clientes')
+
+        cy.get('#comp-kwv3f13i > h1 > span > span:nth-child(2)')
+            .should('have.text', '_')
+    })
+})
